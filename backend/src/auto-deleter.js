4759bb739c0e2f4deb0e8ab494fc88c5b92437d2
Sam Cutter <samuelcutter27@gmail.com>
2023-02-25 08:34:08 +0000

    Added automatic clip deletion after 5 minutes

--- backend/src/auto-deleter.js
+++ backend/src/auto-deleter.js
@@ -1,4 +1,5 @@
 import { initPocketBase } from "./pocketbase.js";
+import PriorityQueue from "priorityqueuejs";
 
 let pb;
 
@@ -13,4 +14,32 @@ pb.collection("clips").subscribe("*", (event) => {
   if (event.action != "create") return;
 
   console.log(`Record with id ${event.record.id} created!`);
+
+  addClipID(event.record.id);
 });
+
+const clipsQueue = new PriorityQueue((a, b) => a.priority - b.priority);
+
+function addClipID(clipID) {
+  const expirationTime = new Date().getTime() + 5 * 60 * 1000;
+  clipsQueue.enq({ item: clipID, priority: expirationTime });
+}
+
+function removeExpiredClips() {
+  const now = new Date().getTime();
+
+  while (!clipsQueue.isEmpty()) {
+    const clipID = clipsQueue.peek().item;
+    const expirationTime = clipsQueue.peek().priority;
+
+    if (now >= expirationTime) {
+      pb.collection("clips").delete(clipID);
+      console.log(`Record with id ${clipID} automatically deleted`);
+      clipsQueue.deq();
+    } else {
+      break;
+    }
+  }
+}
+
+setInterval(removeExpiredClips, 1000);
