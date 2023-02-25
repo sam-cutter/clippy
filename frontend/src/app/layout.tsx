ba97441a6c7c7b4da0ceea6ce367640dc93d2506
Sam Cutter <samuelcutter27@gmail.com>
2023-02-25 14:59:51 +0000

    Installed Next.js for frontend

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,18 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Clippy",
+  description: "Shared clipboard",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
